1c6a73c783fc8a415f3270e46c1a9bd17d011a49
Elderberry <urrutiaeric6@gmail.com>
2024-03-09 10:32:43 -0600

    Update Goods.h

--- Goods.h
+++ Goods.h
@@ -1,1 +1,25 @@
+#ifndef GOODS_H
+#define GOODS_H
+#include <iostream>
+#include <vector>
 
+using namespace std;
+
+class Good {
+    private:
+    bool goodAvailable = false;
+    int totalAvailableGoods = 0;
+    int totalGoodsSent = 0;
+
+    public:
+        
+        void setGoodAvailable( bool x);
+        int setAvailableGoods(int x);
+        int getGoodAvailable();
+        int setGoodsSent(int x);
+
+        
+
+};
+
+#endif
